d942c9b525494af8251b084f14ef7976e829ce6e
cmc3cn <59194618@qq.com>
2020-06-17 15:57:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ function reverse(promise) {
 module.exports = function promiseAny(promiseArr) {
     return reverse(Promise.all(promiseArr.map(reverse)));
 };
+
+// 一个更酷的版本： 两行实现
+
+Promise.any = Promise.any || arr => {
+	let reverse = promise => new Promise((resolve, reject) => Promise.resolve(promise).then(reject, resolve));
+    return reverse(Promise.all(arr.map(reverse)));
+};
